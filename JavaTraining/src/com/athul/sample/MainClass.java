b15ab7a7e66992cb334d73faf34977762e9e504f
Shinigami <31045202+konzerntech@users.noreply.github.com>
2020-05-14 00:49:35 +0530

    Set theme jekyll-theme-slate
    Change-Id: Ib8d4bf3714d9d8c6125286b437d08660f8a205cb
    Signed-off-by: Athul <Athul@admin>

---
+++ JavaTraining/src/com/athul/sample/MainClass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.athul.sample;
+
+/**
+ * @author Athul
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" this is created by atthuol");
+
+	}
+
+}
